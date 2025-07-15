68a3184e018562f60fe7e2fd5d529e605fe6f4b0
irrolliey <bradenbusolo1@gmail.com>
2025-07-15 17:10:58 +0300

    configure prisma in docker, and made first prisma migration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { AuthService } from './auth.service';
-import { Controller, Post } from '@nestjs/common';
-
+import { Controller, Post,Body } from '@nestjs/common';
+import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body('email') email:string,@Body('password') password:string) {
+    console.log({
+      email,
+      password,
+    });
     return this.authService.signup();
   }
 
